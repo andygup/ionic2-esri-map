e796149d63dfa398d5a1ad81681cb5386e863b0a
Andy Gup <agup2001@msn.com>
2018-07-20 11:14:39 -0600

    remove gps code - multiple users asked for simplifiction.

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -20,41 +20,6 @@ export class HomePage implements OnInit{
       // Reference: https://ionicframework.com/docs/api/platform/Platform/#ready
       await this.platform.ready();
 
-      let latitude: number = 0, longitude: number = 0;
-
-      const options = {
-        enableHighAccuracy: true, // use any allowed location provider
-        timeout: 60000            // it can take quite a while for a cold GPS to warm up
-      };
-
-      navigator.geolocation.watchPosition( position=> {
-
-          latitude = position.coords.latitude;
-          longitude = position.coords.longitude;
-
-          // Center map after it has been initialized
-          if(this.mapView != null) {
-            console.log("Centering map: " + latitude + ", " + longitude);
-            this.mapView.center = [longitude, latitude];
-          }
-
-        }, error => {
-
-          switch(error.code) {
-            case error.PERMISSION_DENIED:
-              console.error("User denied the request for Geolocation.");
-              break;
-            case error.POSITION_UNAVAILABLE:
-              console.error("Location information is unavailable.");
-              break;
-            case error.TIMEOUT:
-              console.error("The request to get user location timed out.");
-              alert("Unable to start geolocation. Check application settings.");
-              break;
-          }
-        }, options
-      );
-
       // Load the mapping API modules
       return loadModules([
         'esri/Map',
